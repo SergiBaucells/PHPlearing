42128e29e1c3b9bef7282c8bc050e38960747440
SergiBaucells <sergibaucells@iesebre.com>
2017-09-26 20:51:48 +0200

    20170926

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -5,19 +5,31 @@ namespace App\Controllers;
 class PagesController{
 
     public function home(){
-        require 'app/views/home.view.php';
+
+//        require 'app/views/home.view.php';
+        return view('home');
+
     }
 
     public function news(){
-        require 'app/views/news.view.php';
+
+//        require 'app/views/news.view.php';
+        return view('news');
+
     }
 
     public function contact(){
-        require 'app/views/contact.view.php';
+
+//        require 'app/views/contact.view.php';
+        return view('contact');
+
     }
 
     public function about(){
-        require 'app/views/about.view.php';
+
+//        require 'app/views/about.view.php';
+        return view('about');
+
     }
 
 }
